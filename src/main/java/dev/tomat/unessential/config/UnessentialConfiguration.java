4a87ad5f7fbc7bfd76398e1d7b0c5e676b82f9ff
Steviegt6 <xxlennygamerxx@gmail.com>
2022-03-19 21:35:39 -0700

    Customizable Vigilance colors.

--- src/main/java/dev/tomat/unessential/config/UnessentialConfiguration.java
+++ src/main/java/dev/tomat/unessential/config/UnessentialConfiguration.java
@@ -1,14 +1,21 @@
 package dev.tomat.unessential.config;
 
+import dev.tomat.unessential.UnessentialMod;
 import gg.essential.gui.EssentialPalette;
 import gg.essential.vigilance.Vigilant;
 import gg.essential.vigilance.data.Property;
 import gg.essential.vigilance.data.PropertyType;
+import gg.essential.vigilance.gui.VigilancePalette;
 
 import java.awt.*;
 import java.io.File;
 
 public class UnessentialConfiguration extends Vigilant {
+
+    /** PRIVACY **/
+
+    // region
+
     @Property(
             type = PropertyType.CHECKBOX,
             name = "unessential.config.tos.name",
@@ -16,6 +23,12 @@ public class UnessentialConfiguration extends Vigilant {
             category = "unessential.config.category.privacy"
     )
     public boolean acceptedEssentialTOS = false;
+
+    // endregion
+
+    /** ESSENTIAL VISUALS **/
+
+    // region
 
     @Property(
             type = PropertyType.COLOR,
@@ -152,6 +165,182 @@ public class UnessentialConfiguration extends Vigilant {
             category = "unessential.config.category.visuals"
     )
     public Color sentMessageText = EssentialPalette.SENT_MESSAGE_TEXT;
+
+    // endregion
+
+    /** VIGILANCE VISUALS **/
+
+    // region
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.accent.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color accent = VigilancePalette.INSTANCE.getAccent();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.background.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color background = VigilancePalette.INSTANCE.getBackground();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.bright_divider.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color brightDivider = VigilancePalette.INSTANCE.getBrightDivider();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.bright_highlight.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color brightHighlight = VigilancePalette.INSTANCE.getBrightHighlight();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.bright_text.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color brightText = VigilancePalette.INSTANCE.getBrightText();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.dark_background.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color darkBackground = VigilancePalette.INSTANCE.getDarkBackground();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.dark_divider.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color darkDivider = VigilancePalette.INSTANCE.getDarkDivider();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.dark_highlight.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color darkHighlight = VigilancePalette.INSTANCE.getDarkHighlight();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.dark_text.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color darkText = VigilancePalette.INSTANCE.getDarkText();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.disabled.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color disabled = VigilancePalette.INSTANCE.getDisabled();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.divider.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color divider = VigilancePalette.INSTANCE.getDivider();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.highlight.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color highlight = VigilancePalette.INSTANCE.getHighlight();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.light_background.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color lightBackground = VigilancePalette.INSTANCE.getLightBackground();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.mid_text.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color midText = VigilancePalette.INSTANCE.getMidText();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.modal_background.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color modalBackground = VigilancePalette.INSTANCE.getModalBackground();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.outline.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color outline = VigilancePalette.INSTANCE.getOutline();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.scroll_bar.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color scrollBar = VigilancePalette.INSTANCE.getScrollBar();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.search_bar_background.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color searchBarBackground = VigilancePalette.INSTANCE.getSearchBarBackground();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.success.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color success = VigilancePalette.INSTANCE.getSuccess();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.transparent.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color transparent = VigilancePalette.INSTANCE.getTransparent();
+
+    @Property(
+            type = PropertyType.COLOR,
+            name = "unessential.config.warning.name",
+            description = "unessential.config.color_override_vigilance.description",
+            category = "unessential.config.category.vigilance_visuals"
+    )
+    public Color warning = VigilancePalette.INSTANCE.getWarning();
+
+    // endregion
 
     public UnessentialConfiguration() {
         super(new File("./config/unessential_config.toml"));
--- src/main/java/dev/tomat/unessential/elementa/constraints/ColorOverrideConstraint.java
+++ src/main/java/dev/tomat/unessential/elementa/constraints/ColorOverrideConstraint.java
@@ -2,6 +2,7 @@ package dev.tomat.unessential.elementa.constraints;
 
 import dev.tomat.unessential.UnessentialMod;
 import gg.essential.gui.EssentialPalette;
+import gg.essential.vigilance.gui.VigilancePalette;
 
 import java.awt.*;
 
@@ -11,6 +12,11 @@ import java.awt.*;
  */
 public final class ColorOverrideConstraint {
     public static Color getOverriddenColor(Color input) {
+        // TODO: If someone sets an Essential color to a default Vigilance one, this will cause clashing.
+        return getVigilanceColor(getEssentialColor(input));
+    }
+
+    public static Color getEssentialColor(Color input) {
         if (input == EssentialPalette.ACCENT_HOVER) {
             return UnessentialMod.INSTANCE.CONFIG.accentHover;
         }
@@ -77,6 +83,94 @@ public final class ColorOverrideConstraint {
 
         if (input == EssentialPalette.SENT_MESSAGE_TEXT) {
             return UnessentialMod.INSTANCE.CONFIG.sentMessageText;
+        }
+
+        return input;
+    }
+
+    public static Color getVigilanceColor(Color input) {
+        if (input == VigilancePalette.INSTANCE.getAccent()) {
+            return UnessentialMod.INSTANCE.CONFIG.accent;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getBackground()) {
+            return UnessentialMod.INSTANCE.CONFIG.background;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getBrightDivider()) {
+            return UnessentialMod.INSTANCE.CONFIG.brightDivider;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getBrightHighlight()) {
+            return UnessentialMod.INSTANCE.CONFIG.brightHighlight;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getBrightText()) {
+            return UnessentialMod.INSTANCE.CONFIG.brightText;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDarkBackground()) {
+            return UnessentialMod.INSTANCE.CONFIG.darkBackground;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDarkDivider()) {
+            return UnessentialMod.INSTANCE.CONFIG.darkDivider;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDarkHighlight()) {
+            return UnessentialMod.INSTANCE.CONFIG.darkHighlight;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDarkText()) {
+            return UnessentialMod.INSTANCE.CONFIG.darkText;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDisabled()) {
+            return UnessentialMod.INSTANCE.CONFIG.disabled;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getDarkDivider()) {
+            return UnessentialMod.INSTANCE.CONFIG.darkDivider;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getHighlight()) {
+            return UnessentialMod.INSTANCE.CONFIG.highlight;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getLightBackground()) {
+            return UnessentialMod.INSTANCE.CONFIG.lightBackground;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getMidText()) {
+            return UnessentialMod.INSTANCE.CONFIG.midText;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getModalBackground()) {
+            return UnessentialMod.INSTANCE.CONFIG.modalBackground;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getOutline()) {
+            return UnessentialMod.INSTANCE.CONFIG.outline;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getScrollBar()) {
+            return UnessentialMod.INSTANCE.CONFIG.scrollBar;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getSearchBarBackground()) {
+            return UnessentialMod.INSTANCE.CONFIG.searchBarBackground;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getSuccess()) {
+            return UnessentialMod.INSTANCE.CONFIG.success;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getTransparent()) {
+            return UnessentialMod.INSTANCE.CONFIG.transparent;
+        }
+
+        if (input == VigilancePalette.INSTANCE.getWarning()) {
+            return UnessentialMod.INSTANCE.CONFIG.warning;
         }
 
         return input;
